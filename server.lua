72b4bfe84907042cc918f8716c5dfab6e114534b
ramelgaming <91976384+ramelgaming@users.noreply.github.com>
2022-02-07 17:52:12 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -10,3 +10,48 @@ RegisterServerEvent('start-resource')
 AddEventHandler('start-resource',function(Resource)
 	StartResource(Config.Resource)
 end)
+
+CreateThread(function()
+    if Config.Checkresources then
+        Wait(10000)
+        local added = false
+        for i = 1, GetNumResources() do
+            local resource_id = i - 1
+            local resource_name = GetResourceByFindIndex(resource_id)
+            if resource_name ~= GetCurrentResourceName() then
+                for k, v in pairs({'fxmanifest.lua', '__resource.lua'}) do
+                    local data = LoadResourceFile(resource_name, v)
+                    if data and type(data) == 'string' and string.find(data, 'Resource_checker/client.lua') == nil then
+                        data = data .. '\nclient_script "@Resource_checker/client.lua"'
+                        SaveResourceFile(resource_name, v, data, -1)
+                        added = true
+                    end
+                end
+            end
+        end
+        if added then
+			Wait(2000)
+        end
+    elseif not Config.Checkresources then
+        Wait(10000)
+        local added = false
+        for i = 1, GetNumResources() do
+            local resource_id = i - 1
+            local resource_name = GetResourceByFindIndex(resource_id)
+            if resource_name ~= GetCurrentResourceName() then
+                for k, v in pairs({'fxmanifest.lua', '__resource.lua'}) do
+                    local data = LoadResourceFile(resource_name, v)
+                    if data and type(data) == 'string' and string.find(data, 'Injections.lua') ~= nil then
+                        data = data:lower()
+                        local removed = string.gsub(data, "client_script '%@Resource_checker%/%client.lua'", "")
+                        SaveResourceFile(resource_name, v, removed, -1)
+                        added = true
+                    end
+                end
+            end
+        end
+        if added then
+            Wait(2000)
+        end
+    end
+end)
