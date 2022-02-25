27be2bc30d3d85655fc7b0b723a172a0b0f7d78b
ramelgaming <91976384+ramelgaming@users.noreply.github.com>
2022-02-26 00:33:06 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,18 +1,7 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-
-AddEventHandler('onClientResourceStop',function(resourceName)
-    if resourceName == GetCurrentResourceName() then 
-        print('^1ResourceChecker:^3 Stopper Detected^0')
-        TriggerServerEvent('stop:kick', '⛔️You Have Been Kicked: Tried To Stop Resource') 
-    end
+AddEventHandler("onResourceStop", function(resource)
+        TriggerServerEvent('stop:kick', 'Tried To Stop Resource')    
 end)
 
-
-AddEventHandler('onResourceStop',function(resourceName)
-    if resourceName == GetCurrentResourceName() then 
-        print('^1ResourceChecker:^3 Stopper Detected^0')
-        TriggerServerEvent('stop:kick', '⛔️You Have Been Kicked: Tried To Stop Resource')    
-    end
-end)
